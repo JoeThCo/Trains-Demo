da8bf0f7005e7a11bebc81053ddb05246b5a1e4d
Joe Colley <josephthco@gmail.com>
2024-09-17 13:15:48 -0400

    boiler plate

--- Trains/Trains/Assets/Scripts/GraphGenerator.cs
+++ Trains/Trains/Assets/Scripts/GraphGenerator.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class GraphGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private SplineContainer splineContainer;
+    public void SplitGraph()
     {
-        
+        splineContainer = FindObjectOfType<SplineContainer>();
     }
 }
---
+++ Trains/Trains/Assets/Scripts/GraphGeneratorEditor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GraphGenerator))]
+public class GraphGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+        GraphGenerator graphGenerator = target as GraphGenerator;
+        if (GUILayout.Button("Split Splines")) 
+        {
+            graphGenerator.SplitGraph();    
+        }
+    }
+}
